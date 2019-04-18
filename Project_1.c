b168be8f4ec31be286410dfeae99387b8f21e64d
Riley Smith <c3328334@uon.edu.au>
2019-04-18 04:12:36 +0000

    code plan

--- Project_1.c
+++ Project_1.c
@@ -1,4 +1,13 @@
+//define the rotation cipher function
+
 # include <stdio.h>
 int main(){
-    printf("asdf");
+    
+//ask user for the text to encrypt
+
+//convert users text to a variable
+
+//print the ciphered text
 }
+
+//rotation cipher definition
